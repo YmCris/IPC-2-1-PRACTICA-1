d1f97b24dccd688c437c098756ec343c3484ba50
Cris <ymcrisystems@gmail.com>
2025-08-11 11:57:09 -0600

    HyruleEvents

---
+++ HyruleEvents/src/main/java/ymcris/ipc2/practica1/hyruleevents/HyruleEvents.java
@@ -0,0 +1,12 @@
+package ymcris.ipc2.practica1.hyruleevents;
+
+/**
+ *
+ * @author YmCris
+ */
+public class HyruleEvents {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
